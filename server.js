be770ecc0f6a244f73ae033ed41abffbbf594e58
Tim <tim@tabdeveloper.com>
2013-11-08 19:16:42 -0500

    Tim testing.

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ http.createServer(function (req, res) {
   // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
   var voteko = '<iframe src="http://nodeknockout.com/iframe/glazed-nodenuts" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
 
+  <!-- I'm a hipster -->
+  voteko += "<!-- tim deploy good for great win! -->";
+
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<html><body>' + voteko + '</body></html>\n');
 }).listen(port, function(err) {
