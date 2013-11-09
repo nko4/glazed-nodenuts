fba7a6648e4e7cf132406549e13c6290d9b97739
Kyle Robinson Young <kyle@dontkry.com>
2013-11-09 09:25:13 -0800

    Add URL to shim

--- static/js/shim.js
+++ static/js/shim.js
@@ -1,2 +1,3 @@
 navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 window.AudioContext = (window.AudioContext || window.webkitAudioContext || navigator.mozAudioContext || navigator.msAudioContext);
+window.URL = (window.URL || window.webkitURL || window.mozURL || window.msURL || window.oURL);
