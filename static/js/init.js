c8b698ef1430ff1a9c5e62db5f26c6a254848137
Tim <tim@tabdeveloper.com>
2013-11-08 23:57:02 -0500

    Lyrics highlighting with correct timing.

--- static/js/init.js
+++ static/js/init.js
@@ -2,18 +2,48 @@ window.lyrics = null;
 
 // Fetch lyrics.
 $.getJSON('/api/lyrics', function(lyrics) {
-  var template = _.template($("#lyrics").html());
+  var template = _.template($('#lyrics').html());
 
   // Globalize.
   window.lyrics = lyrics;
 
-  $("some-lyrics").html(template({ lyrics: _(lyrics) }));
+  $('some-lyrics').html(template({ lyrics: _(lyrics) }));
 
   simulatePlaying();
 });
 
 function simulatePlaying() {
-  var time = +new Date();
+  var startTime = Date.now();
+  var start = Number(lyrics[0].playTime);
+  var stop = Number(lyrics[lyrics.length-1].playTime);
 
-  console.log(time);
+  // Awesome variable name.
+  var $lyrics = $('some-lyrics > a-lyric');
+
+  // Maintain it like this for now.
+  var index = 0;
+
+  var poller = window.setInterval(function() {
+    var elapsed = (Date.now() - startTime)/1000;
+
+    // Bail out if out of lyrics.
+    if (!lyrics.length) {
+      return window.clearInterval(poller);
+    }
+
+    if ((lyrics[index].playTime/1000) <= elapsed) {
+      $lyrics[index].classList.add('active');
+
+      if ($lyrics[index-1]) {
+        $lyrics[index-1].classList.remove('active');
+      }
+
+      // Just like papa crock taught me!
+      index = index + 1;
+    }
+
+    console.log(lyrics[index].playTime/1000, elapsed);
+  }, 100);
+
+  //console.log(time, lyrics[0].playTime);
 }
