a4d7639690f055639edb5a085fc5e6d64f82d534
Tim <tim@tabdeveloper.com>
2013-11-08 23:01:28 -0500

    Refactored API endpoint and globalized lyrics.

--- static/js/init.js
+++ static/js/init.js
@@ -1,6 +1,11 @@
+window.lyrics = null;
+
 // Fetch lyrics.
 $.getJSON('/api/lyrics', function(lyrics) {
   var template = _.template($("#lyrics").html());
+
+  // Globalize.
+  window.lyrics = lyrics;
 
   $("some-lyrics").html(template({ lyrics: _(lyrics) }));
 });
