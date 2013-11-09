da5aadae40b5813a2972f219925eb263c36327c1
Kyle Robinson Young <kyle@dontkry.com>
2013-11-09 10:28:48 -0800

    Add/remove people when connecting

--- static/js/room.js
+++ static/js/room.js
@@ -1,3 +1,6 @@
+// disable rtc logging
+window.skipRTCMultiConnectionLogs = true;
+
 window.Person = (function() {
 
   function Person(opts) {
@@ -6,7 +9,16 @@ window.Person = (function() {
     this.id = opts.id || Math.round(Math.random() * 999999999) + 999999999;
     this.name = opts.name || '???';
     this.status = opts.status || 'idontknow'; // singing | clapping | listening | idontknow
+    this.mediaElement = null;
   }
+
+  Person.prototype.toData = function() {
+    return {
+      id: this.id,
+      name: this.name,
+      status: this.status,
+    }
+  };
 
   return Person;
 }());
@@ -15,18 +27,22 @@ window.Controls = (function() {
 
   function Controls(opts) {
     if (!(this instanceof Controls)) return new Controls(opts);
+    var self = this;
+
     opts = opts || {};
+    this.room = opts.room;
     this.element = opts.element || $('<div/>');
 
     this.connection = new RTCMultiConnection();
-    this.sessionStarted = false;
+    //this.sessionStarted = false;
     this.connection.session = { audio: true };
 
     this.connection.openSignalingChannel = function(config) {
       var SIGNALING_SERVER = 'https://www.webrtc-experiment.com:2015/';
       //var SIGNALING_SERVER = 'https://localhost:2015/';
       var channel = config.channel || this.channel || location.hash.substr(1);
-      var sender = Math.round(Math.random() * 999999999) + 999999999;
+      //var sender = Math.round(Math.random() * 999999999) + 999999999;
+      var sender = '1234';
 
       io.connect(SIGNALING_SERVER).emit('new-channel', {
         channel: channel,
@@ -48,30 +64,28 @@ window.Controls = (function() {
       socket.on('message', config.onmessage);
     };
 
+    // WHEN PEOPLE HAVE ARRIVED
     this.connection.onstream = function(e) {
-      sessionStarted = true;
-      audiosContainer.insertBefore(e.mediaElement, audiosContainer.firstChild);
-      rotateAudio(e.mediaElement);
+      console.log('person should add', e);
+      if (e.extra && e.extra.person && e.extra.person.id) {
+        self.room.set(e.extra.person.id, {
+          name: e.extra.person.name,
+          status: e.extra.person.status,
+          mediaElement: e.mediaElement,
+        });
+      }
     };
 
-    function rotateAudio(mediaElement) {
-      mediaElement.style[navigator.mozGetUserMedia ? 'transform' : '-webkit-transform'] = 'rotate(0deg)';
-      setTimeout(function() {
-        mediaElement.style[navigator.mozGetUserMedia ? 'transform' : '-webkit-transform'] = 'rotate(360deg)';
-      }, 1000);
-    }
-
+    // WHEN PEOPLE HAVE LEFT
     this.connection.onstreamended = function(e) {
-      e.mediaElement.style.opacity = 0;
-      rotateAudio(e.mediaElement);
-      setTimeout(function() {
-        if (e.mediaElement.parentNode) {
-          e.mediaElement.parentNode.removeChild(e.mediaElement);
-        }
-      }, 1000);
+      console.log('person should leave', e);
+      if (e.extra && e.extra.person && e.extra.person.id) {
+        self.room.remove(e.extra.person.id);
+      }
     };
 
-    // start looking for connections
+    // START LOOKING FOR CONNECTIONS
+    // TODO: This probably shouldnt happen unless the user wants to sing/clap
     this.connection.connect();
   }
 
@@ -93,28 +107,31 @@ window.Controls = (function() {
   // When a user wants to sing
   Controls.prototype.onWantToSing = function(person) {
     console.log('I want to sing')
+    person.status = 'singing';
     connection.extra = {
-      'session-name': 'Anonymous'
+      'session-name': 'Anonymous',
+      person: person.toData(),
     };
     connection.open();
-    person.status = 'singing';
     this.choices(person);
   };
 
   // When a user wants to clap
   Controls.prototype.onWantToClap = function(person) {
     console.log('I want to clap')
+    person.status = 'clapping';
     connection.extra = {
-      'session-name': 'Anonymous'
+      'session-name': 'Anonymous',
+      person: person.toData(),
     };
     connection.open();
-    person.status = 'clapping';
     this.choices(person);
   };
 
   // When a user just wants to listen
   Controls.prototype.onWantToListen = function(person) {
     console.log('I want to listen')
+    // TODO: dont prompt for mic, just add other streams
   };
 
   Controls.prototype._createAction = function(type, person, text) {
@@ -154,6 +171,7 @@ window.Room = (function() {
   function Room(opts) {
     if (!(this instanceof Room)) return new Room(opts);
     opts = opts || {};
+    this.Person = opts.Person || window.Person;
     this.element = opts.element || $('<ul/>');
     this.element.addClass('room');
     this.prefix = 'person';
@@ -167,6 +185,7 @@ window.Room = (function() {
   }
 
   Room.prototype.add = function(person) {
+    if (!(person instanceof Person)) person = new this.Person(person);
     var li = $('<li/>');
     li.attr({
       id: this.prefix + person.id,
@@ -176,24 +195,42 @@ window.Room = (function() {
     li.identicon5({ size: 30 });
     this.element.append(li);
     this._people[this.prefix + person.id] = person;
+    return person;
+  };
+
+  Room.prototype.set = function(id, data) {
+    var person = this._people[this.prefix + id];
+    if (!person) person = this.add(data);
+    var el = this.element.find('#' + this.prefix + id);
+    el.attr('title', person.name + ' - ' + person.status);
   };
 
   Room.prototype.remove = function(id) {
     this.element.find('#' + this.prefix + id).remove();
-    delete this._people[this.prefix + person.id];
+    delete this._people[this.prefix + id];
+    return id;
+  };
+
+  Room.prototype.find = function(id) {
+    return (this._people[this.prefix + id]) ? this._people[this.prefix + id] : false;
   };
 
   return Room;
 }());
 
-var controls = new Controls({ element: $('#controls') });
-var dude = new Person({ name: 'dude' });
 
-controls.choices(dude);
+
+
+
 
 /* Usage */
 var room = new Room();
+var controls = new Controls({ element: $('.controls-ui'), room: room });
+var dude = new Person({ name: 'dude' });
+
+controls.choices(dude);
 room.add(dude);
+
 $('body').prepend(room.element);
 
 // var peeps = room.people;
