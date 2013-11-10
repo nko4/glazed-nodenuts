6e14901934e781523831e4e907532e7215b4138a
Kyle Robinson Young <kyle@dontkry.com>
2013-11-09 23:02:36 -0800

    Fix copy/paste fail

--- static/js/shim.js
+++ static/js/shim.js
@@ -1,3 +1,3 @@
 navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-window.AudioContext = (window.AudioContext || window.webkitAudioContext || navigator.mozAudioContext || navigator.msAudioContext);
+window.AudioContext = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext);
 window.URL = (window.URL || window.webkitURL || window.mozURL || window.msURL || window.oURL);
