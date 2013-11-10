b20b85f0bf3e4a1be24868e091035ab04ea78f62
Kyle Robinson Young <kyle@dontkry.com>
2013-11-09 21:12:27 -0800

    Emit clap when over 15

--- static/js/connection.js
+++ static/js/connection.js
@@ -60,6 +60,7 @@ function createUser(e) {
 
   var clap = new Clap();
   var node = clap.detect(context.createMediaStreamSource(e.stream), context, function(err, average) {
+    if (average > 15) socket.emit('clap');
     userEl.get(0).style.backgroundColor = 'hsl(170, ' + average + '%, 50%)';
   });
   node.connect(context.destination);
