9730c9769587bcefa1162bb4e2ee64189d5b85cd
Jenn Schiffer <jenn@pancaketheorem.com>
2013-11-09 19:18:33 -0500

    prelim vol slider

--- static/js/init.js
+++ static/js/init.js
@@ -14,6 +14,7 @@
       this.lyrics = $('some-lyrics');
       this.monitor = $('.tv-contents');
       this.songTitle = $('.tv-title');
+      this.volumeSlider = $('#vol');
     }
   };
 
@@ -200,4 +201,10 @@
 
   // Kick off the application by starting the current song.
   playCurrentSong();
+  
+  // Dom events
+  dom.volumeSlider.change(function(){
+    MIDI.setVolume($(this).val());
+    console.log($(this).val());
+  });
 })(this);
